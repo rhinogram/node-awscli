d890ef6ef3a0a477d453935394d183217dc59e84
Grady Rogers <rogersgt819@gmail.com>
2017-10-06 10:04:35 -0400

    ecs and merege-yaml-cli

--- dependencies.sh
+++ dependencies.sh
@@ -5,5 +5,9 @@ sudo apt-get update && sudo apt-get install -qq -y python-pip libpython-dev
 curl -O https://bootstrap.pypa.io/get-pip.py && python get-pip.py
 sudo pip install -q awscli --upgrade
 
-# echo 'installing global npm packages...'
-# sudo npm install -g aws-sdk colors
+# ecs-cli
+sudo curl -o /usr/local/bin/ecs-cli https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-linux-amd64-latest
+sudo chmod +x /usr/local/bin/ecs-cli
+
+# global npm
+sudo npm install -g merge-yaml-cli
