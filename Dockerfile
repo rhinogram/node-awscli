ed072bf1f497897c3876fa6fb4726f3e82e166d7
Grady Rogers <rogersgt819@gmail.com>
2017-10-30 13:23:53 -0400

    ecs-deploy

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,9 @@ FROM circleci/node:6.11.2
 LABEL maintainer="Rhinogram, LLC"
 LABEL description="Basic Docker image with Node and AWS CLI for CircleCi workspaces"
 
-ADD dependencies.sh ~/
-WORKDIR ~
+RUN sudo mkdir -p /dir
+ADD dependencies.sh test.js /dir/
+WORKDIR /dir
 RUN sudo apt-get install apt-utils
 RUN sudo chmod +x ./dependencies.sh
 RUN ./dependencies.sh
--- dependencies.sh
+++ dependencies.sh
@@ -2,12 +2,16 @@
 
 echo "installing AWS CLI..."
 sudo apt-get update && sudo apt-get install -qq -y python-pip libpython-dev
-curl -O https://bootstrap.pypa.io/get-pip.py && python get-pip.py
+sudo curl -O https://bootstrap.pypa.io/get-pip.py && sudo python get-pip.py
 sudo pip install -q awscli --upgrade
 
 # ecs-cli
 sudo curl -o /usr/local/bin/ecs-cli https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-linux-amd64-latest
 sudo chmod +x /usr/local/bin/ecs-cli
 
+# ecs-deploy
+sudo pip install ecs-deploy -y
+
 # global npm
 sudo npm install -g merge-yaml-cli
+sudo npm install -g aws-sdk
