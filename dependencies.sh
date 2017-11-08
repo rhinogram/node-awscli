ceabc00cf1fbd2a0c014facfa446ece1c244b0a3
Grady Rogers <rogersgt819@gmail.com>
2017-11-08 17:47:54 -0500

    jq ecs-cli

--- dependencies.sh
+++ dependencies.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-echo "installing AWS CLI..."
-sudo apt-get update && sudo apt-get install -qq -y python-pip libpython-dev
+echo "installing dependencies..."
+sudo apt-get update 
+sudo apt-get install -qq -y python-pip libpython-dev
 sudo curl -O https://bootstrap.pypa.io/get-pip.py && sudo python get-pip.py
 sudo pip install -q awscli --upgrade
+
+# jq json parser
+sudo apt-get install jq
 
 # ecs-cli
 sudo curl -o /usr/local/bin/ecs-cli https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-linux-amd64-latest
@@ -12,4 +16,3 @@ sudo chmod +x /usr/local/bin/ecs-cli
 # global npm
 sudo npm install -g merge-yaml-cli
 sudo npm install -g aws-sdk
-sudo npm install -g jq-cli-wrapper
