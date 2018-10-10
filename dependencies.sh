346f184542c869793882d79d875a4815252eb661
Grady Rogers <rogersgt819@gmail.com>
2018-10-10 18:42:24 -0400

    8.11 cleanup

--- dependencies.sh
+++ dependencies.sh
@@ -9,15 +9,8 @@ sudo pip install -q awscli==1.11.115 --upgrade # lock version
 # jq json parser
 sudo apt-get install jq
 
-# ecs-cli
-sudo curl -o /usr/local/bin/ecs-cli https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-linux-amd64-latest
-sudo chmod +x /usr/local/bin/ecs-cli
-
 # global npm
 sudo npm install -g merge-yaml-cli
 sudo npm install -g aws-sdk
 sudo npm install -g babel-cli
 sudo npm install -g api-console-cli
-
-# update docker cash
-# docker pull node:8.9
